3cc5c260d63c59e8af70bddc7167382a5f851fca
Ekaterina-El <hackerrussia@yandex.ru>
2023-02-01 19:51:14 +0500

    Init

---
+++ app/src/main/java/el/ka/speechart/MainActivity.kt
@@ -0,0 +1,12 @@
+package el.ka.speechart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setTheme(R.style.Theme_SpeechArt)
+    setContentView(R.layout.activity_main)
+  }
+}
