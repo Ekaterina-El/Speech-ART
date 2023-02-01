e4970fc5c4394a40813e9a4ec3e90389b7a2e812
Ekaterina-El <hackerrussia@yandex.ru>
2023-02-02 01:41:12 +0500

    Navigate to login screen after success sign up

--- app/src/main/java/el/ka/speechart/view/ui/auth/SignUpFragment.kt
+++ app/src/main/java/el/ka/speechart/view/ui/auth/SignUpFragment.kt
@@ -5,15 +5,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import el.ka.speechart.R
 import el.ka.speechart.databinding.SingUpFragmentBinding
+import el.ka.speechart.other.Action
 import el.ka.speechart.view.ui.BaseFragment
 import el.ka.speechart.viewModel.SignUpViewModel
 
 class SignUpFragment : BaseFragment() {
   private lateinit var viewModel: SignUpViewModel
   private lateinit var binding: SingUpFragmentBinding
+
+  private val externalActionObserver = Observer<Action?> {
+    if (it == Action.GO_NEXT) findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+  }
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -37,11 +44,13 @@ class SignUpFragment : BaseFragment() {
     super.onResume()
     viewModel.work.observe(viewLifecycleOwner, workObserver)
     viewModel.error.observe(viewLifecycleOwner, errorObserver)
+    viewModel.externalAction.observe(viewLifecycleOwner, externalActionObserver)
   }
 
   override fun onStop() {
     super.onStop()
     viewModel.work.removeObserver(workObserver)
     viewModel.error.removeObserver(errorObserver)
+    viewModel.externalAction.removeObserver(externalActionObserver)
   }
 }
