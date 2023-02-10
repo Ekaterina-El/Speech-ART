1e31d65c9377c5d6af5389097da9f4289ff19e48
Ekaterina-El <hackerrussia@yandex.ru>
2023-02-11 00:31:34 +0500

    Show level of exercise

--- app/src/main/java/el/ka/speechart/view/adapter/databinding/textDataBinding.kt
+++ app/src/main/java/el/ka/speechart/view/adapter/databinding/textDataBinding.kt
@@ -3,6 +3,7 @@ package el.ka.speechart.view.adapter.databinding
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import el.ka.speechart.other.LevelOfDifficulty
 import el.ka.speechart.other.toMinutesAndSeconds
 
 @BindingAdapter("app:text")
@@ -19,5 +20,11 @@ fun hideView(view: View, value: Any?) {
 @BindingAdapter("app:time")
 fun showTime(textView: TextView, time: Int?) {
   textView.text = time?.toMinutesAndSeconds() ?: ""
+}
+
+@BindingAdapter("app:levelExercise")
+fun showExerciseLevel(textView: TextView, levelOfDifficulty: LevelOfDifficulty?) {
+  if (levelOfDifficulty == null) return
+  textView.text = textView.context.getString(levelOfDifficulty.strRes)
 }
 
